ce91b34e899f85512e8df19d17f78c476d1b5207
Allan Wanderley <awallan259@gmail.com>
2020-10-16 01:16:28 -0300

    Adicionado métodos CRUD

--- 03_Domain/Core/Interfaces/Services/ITemaService.cs
+++ 03_Domain/Core/Interfaces/Services/ITemaService.cs
@@ -1,4 +1,13 @@
+using System.Collections.Generic;
+using Core.Entities;
+
 namespace Core.Interfaces.Services
 {
-    public interface ITemaService { }
+    public interface ITemaService 
+    { 
+        IEnumerable<Tema> Obter(string termo, int? skip, int? take);
+        Tema Obter(int? idTema);
+        Tema Criar(string nome, string descricao);
+        int Remover(int? idTema);
+    }
 }
--- 03_Domain/Services/TemaService.cs
+++ 03_Domain/Services/TemaService.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Core.Entities;
 using Core.Interfaces.Repository;
 using Core.Interfaces.Services;
@@ -8,5 +10,40 @@ namespace Services
     {
         public TemaService(IUnitOfWork unitOfWork, IRepository<Tema> repository) 
             : base(unitOfWork, repository) { }
+
+        public IEnumerable<Tema> Obter(string termo, int? skip, int? take) => 
+            Obter(item => string.IsNullOrEmpty(termo) || item.Nome.StartsWith(termo), skip, take);
+
+        public Tema Obter(int? idTema) =>
+            Obter(item => item.Id == idTema.Value) ?? throw new ArgumentNullException("Tema não encontrado");
+
+        public Tema Criar(string nome, string descricao)
+        {
+            var tema = new Tema(nome, descricao);
+            tema.Validar();
+
+            Adicionar(tema);
+            Salvar();
+
+            return tema;
+        }
+
+        public int Remover(int? idTema)
+        {
+            if(!idTema.HasValue)
+                throw new ArgumentException("É necessário informar o identificador do tema");
+            
+            Tema tema = Obter(item => item.Id == idTema.Value);
+
+            if(tema.EstaRemovido())
+                throw new ArgumentException("Este tema já esta removido");
+            
+            tema.Remover();
+
+            Atualizar(tema);
+            Salvar();
+
+            return tema.Id;
+        }
     }
 }
