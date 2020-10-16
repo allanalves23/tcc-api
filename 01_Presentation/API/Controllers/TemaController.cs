026f54f8ae7d6533e742c1ed97d492996bbc3be9
Allan Wanderley <awallan259@gmail.com>
2020-10-16 01:15:01 -0300

    Criado controller de temas

---
+++ 01_Presentation/API/Controllers/TemaController.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Core.Interfaces.Services;
+using API.Models;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TemaController : ControllerBase
+    {
+        public ITemaService _temaService;
+
+        public TemaController(ITemaService temaService)
+        {
+            _temaService = temaService;
+        }
+
+        [HttpGet]
+        public IActionResult GetTemas(string termo, int? skip = 0, int? take = 10) => 
+            Ok(_temaService.Obter(termo, skip, take).Select(item => new TemaModel(item)));
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetTema(int id) => Ok(new TemaModel(_temaService.Obter(id)));
+
+        [HttpPost]
+        public IActionResult CreateTema([FromBody] TemaModel tema) => 
+            Created("", new TemaModel(_temaService.Criar(tema?.Nome, tema?.Descricao)));
+
+        [HttpDelete("{id:int}")]
+        public IActionResult UpdateTema(int id) => Ok(_temaService.Remover(id));
+    }
+}
