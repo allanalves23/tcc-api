9424f886f57ba4884d57122c9864d7ca2fdb585c
Allan Wanerley Alves <awallan259@gmail.com>
2020-10-19 11:00:56 -0300

    Troca de Emissor e receptor do token + Refatorações na inicialização da estrutura no banco do identity

--- 01_Presentation/API/Security/IdentityInitializer.cs
+++ 01_Presentation/API/Security/IdentityInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using API.Models.Identity;
 using Microsoft.AspNetCore.Identity;
 
@@ -20,54 +21,54 @@ namespace API.Security
             _roleManager = roleManager;
         }
 
-        public void Initialize()
+        public async Task Initialize()
         {
             if (_context.Database.EnsureCreated())
             {
-                if (!_roleManager.RoleExistsAsync(RolesModel.Product).Result)
+                if (!await _roleManager.RoleExistsAsync(RolesModel.Product))
                 {
-                    var resultado = _roleManager.CreateAsync(
-                        new IdentityRole(RolesModel.Product)).Result;
+                    IdentityResult resultado = await _roleManager.CreateAsync(
+                        new IdentityRole(RolesModel.Product));
+
                     if (!resultado.Succeeded)
-                    {
                         throw new Exception(
                             $"Erro durante a criação da role {RolesModel.Product}.");
-                    }
                 }
 
-                CreateUser(
-                    new ApplicationUser()
-                    {
-                        UserName = "awallan259@gmail.com",
-                        Email = "awallan259@gmail.com",
-                        EmailConfirmed = true
-                    }, "Pass123$", RolesModel.Product);
-
-                CreateUser(
-                    new ApplicationUser()
-                    {
-                        UserName = "davi.demk@yahoo.com.br",
-                        Email = "davi.demk@yahoo.com.br",
-                        EmailConfirmed = true
-                    }, "Pass123$", RolesModel.Product);
+                SeedUsers();
             }
         }
-        private void CreateUser(
+        private async Task CreateUser(
             ApplicationUser user,
             string password,
             string initialRole = null)
         {
-            if (_userManager.FindByNameAsync(user.UserName).Result == null)
+            if (await _userManager.FindByNameAsync(user.UserName) == null)
             {
-                var resultado = _userManager
-                    .CreateAsync(user, password).Result;
+                IdentityResult resultado = await _userManager.CreateAsync(user, password);
 
-                if (resultado.Succeeded &&
-                    !String.IsNullOrWhiteSpace(initialRole))
-                {
+                if (resultado.Succeeded && !String.IsNullOrWhiteSpace(initialRole))
                     _userManager.AddToRoleAsync(user, initialRole).Wait();
-                }
             }
+        }
+
+        private async void SeedUsers()
+        {
+            await CreateUser(
+                new ApplicationUser()
+                {
+                    UserName = "awallan259@gmail.com",
+                    Email = "awallan259@gmail.com",
+                    EmailConfirmed = true
+                }, "Pass123$", RolesModel.Product);
+
+            await CreateUser(
+                new ApplicationUser()
+                {
+                    UserName = "davi.demk@yahoo.com.br",
+                    Email = "davi.demk@yahoo.com.br",
+                    EmailConfirmed = true
+                }, "Pass123$", RolesModel.Product);
         }
     }
 }
