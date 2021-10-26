869cafd010c4e80e71aaf978fd34d4c1f22d2172
Allan Wanderley <awallan259@gmail.com>
2021-10-26 00:12:26 -0300

    core: adicionado logging serilog + elastic cloud

--- 01_Presentation/API/Program.cs
+++ 01_Presentation/API/Program.cs
@@ -1,5 +1,8 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace API
 {
@@ -12,6 +15,22 @@ namespace API
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var settings = config.Build();
+                    Serilog.Log.Logger = new LoggerConfiguration()
+                        .Enrich.FromLogContext()
+                        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(settings["Elasticsearch:Uri"]))
+                        {
+                            AutoRegisterTemplate = true,
+                            AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+                            IndexFormat = settings["Elasticsearch:IndexName"]+"-{0:yyyy.MM}",
+                            ModifyConnectionSettings = x => x.BasicAuthentication(settings["Elasticsearch:User"], settings["Elasticsearch:Pass"])
+                        })
+                        .WriteTo.Console()
+                        .CreateLogger();
+                })
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseUrls("http://0.0.0.0");
